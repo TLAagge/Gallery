148c39d48bdeb87c6394fe947092f1daf1ad3db5
TLAagge <battuguldur2017@gmail.com>
2024-12-06 13:59:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,27 @@
 const lightbox = document.getElementById('lightbox');
 const lightboxContent = document.getElementById('lightbox-content');
 const closeLightbox = document.querySelector('.lightbox .close');
+const imageInfo= document.getElementById('image-info');
+const photoLocation=document.getElementById('photo-location');
+
+const imageDetails = {
+    "DSC07242.jpg": "Taken in Mongolia."
+    "DSC07321.jpg": "Taken in Mongolia."
+    "DSC07331.jpg": "Taken in Mongolia."
+    "DSC07425.jpg": "Taken in Mongolia."
+    "DSC07374.jpg": "Taken in Mongolia."
+};
 
 // Open lightbox on image click
 document.querySelectorAll('.lightbox-img').forEach(img => {
     img.addEventListener('click', () => {
+        const imageSrc = img.getAttribute('data-src');
         lightbox.style.display = 'flex';
-        lightboxContent.src = img.getAttribute('data-src');
+        lightboxContent.src = imageSrc;
+
+        // Set the corresponding image info (location)
+        const locationText = imageDetails[imageSrc] || "Location information not available";
+        photoLocation.textContent = locationText;
     });
 });
 
@@ -21,4 +36,3 @@ lightbox.addEventListener('click', (e) => {
         lightbox.style.display = 'none';
     }
 });
-
